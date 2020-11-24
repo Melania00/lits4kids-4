c30f098fccfed6f7fc24e906a78b9fee6fa3922a
Melania <Melania>
2020-11-24 19:59:28 +0200

    save changes

--- js_lessons/dom/script.js
+++ js_lessons/dom/script.js
@@ -37,12 +37,52 @@ console.log(
           if(width > maxWidth && blockResizeStatus === "TO_SMALL"){
             blockResizeStatus = "TO_LARGE";
           }
+
+          renderRedBox.style = `width: ${width}px;`;
       }
 
       setInterval(() => {
           resizeBlockAimation()
-      }, 500);
+      }, 10);
 
+
+
+
+      /* ============== User List ============== */
+
+
+      const userList = document.querySelector("#user-list");
+      const allUserDB = [
+          {
+              name: "John",
+              age: 15
+        },{
+            name: "Sara",
+            age: 18
+        }
+      ];
+
+      initList(allUserDB); //Точка входу
+
+      function createNewElement(user){
+          const element = document.createElement(`li`);
+          element.classList.add("list-style");
+          element.classList.add("list-group");
+
+          element.innerHTML = `
+          <div class="card-wrapper">
+            <h3>${user.name}</h3>
+            <p>${user.age}</p>
+          </div>  
+          `;
+          userList.appendChild(element);
+      }
+
+      function initList(listObject){
+          for(let singleUser of listObject){
+              createNewElement(singleUser)
+          }
+      }
 
 
 
