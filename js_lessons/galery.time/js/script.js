4e2e2c1678165bacf8c1ef4d0ad1360b30cda82e
Melania <Melania>
2020-12-15 19:28:37 +0200

    save changes

---
+++ js_lessons/galery.time/js/script.js
@@ -0,0 +1,29 @@
+console.log('Work')
+
+const controlObject = {
+    galleryMainBase: [{
+        id: 1,
+        url: "https://i2.wp.com/movingtips.wpengine.com/wp-content/uploads/2019/07/new-york2.jpg?fit=1024%2C684&ssl=1",
+        title: "Title 1",
+        description: "Litle description abour the city 1"
+    },{
+        id: 2,
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaDVy0oz5man04q9kEQYDb8flw8MribNFWEA&usqp=CAU",
+        title: "Title 2",
+        description: "Litle description abour the city 2"
+    },{
+        id: 3,
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3peU5B6_8ZqnFbOttelocs04a8coGws3Lqw&usqp=CAU",
+        title: "Title 3",
+        description: "Litle description abour the city 3"
+    }],
+    timerSeasonSets: [],
+    selecrors : {
+        pageTimer: document.querySelector('#page-timer'),
+        galleryDisplayArea: document.querySelector('#gallery-slector')
+        //class selector must be
+    }
+}
+
+
+/**/
